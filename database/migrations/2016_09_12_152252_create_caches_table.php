c41a100ab85093660be258f7fb5b436c977c8835
Pierre Boivin <pierreboivin85@gmail.com>
2016-12-06 16:01:06 -0500

    [FIX] Compatibilité Openshift

--- database/migrations/2016_09_12_152252_create_caches_table.php
+++ database/migrations/2016_09_12_152252_create_caches_table.php
@@ -16,8 +16,7 @@ class CreateCachesTable extends Migration {
 			$table->string('identifier');
 			$table->string('username');
 			$table->longText('data');
-			$table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-			$table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+			$table->timestamps(); // Pour Laravel openshift
 		});
 	}
 
