371c0de5c8340a8fe758e62c2fe0bffa926a815f
Pierre Boivin <pierre.boivin@libeo.com>
2019-01-25 13:38:07 -0500

    Heroku

--- config/database.php
+++ config/database.php
@@ -64,11 +64,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
-            'port' => env('DB_PORT', '3306'),
-            'database' =>  env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'host' => $host,
+            'port' => '3306',
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
